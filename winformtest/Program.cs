6e77feb54e9a95affeda2a30bfb088f0eb35f043
wsm516 <112995519+wsm516@users.noreply.github.com>
2022-12-16 20:15:14 +0900

    Update Program.cs

--- winformtest/Program.cs
+++ winformtest/Program.cs
@@ -8,7 +8,7 @@ namespace winformtest
 {
     internal static class Program
     {
-        /// <summary>
+        /// <summary> 
         /// 해당 애플리케이션의 주 진입점입니다. 
         /// </summary> 
         [STAThread]
